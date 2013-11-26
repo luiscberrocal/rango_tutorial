b0b1f51722575c04d3915dc353ed32715d9ab89a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2013-11-25 21:28:01 -0500

    Changes to the model. Added populate_rango.py to populate the data

--- populate_rango.py
+++ populate_rango.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python', 128,64)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
@@ -24,7 +24,7 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django",64, 32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
@@ -38,7 +38,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks", 32, 16)
 
     add_page(cat=frame_cat,
         title="Bottle",
@@ -57,8 +57,8 @@ def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 # Start execution here!
