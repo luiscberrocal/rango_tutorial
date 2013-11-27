c990a2d92acd60011aece52ddc5069c95cce79fb
Luis Berrocal <lberrocal@pancanal.com>
2013-11-27 08:12:30 -0500

    Se cambio el return de unicode de AcceptanceCriteria para que diera mas informacion. Se cargaron datos de prueba

--- goalmatrix/models.py
+++ goalmatrix/models.py
@@ -11,7 +11,11 @@ class Employee(models.Model):
         return u"%s, %s" % (self.last_name, self.first_name,)
 
 class Goal(models.Model):
-    description = models.CharField(max_length=30)
+    position = models.IntegerField()
+    description = models.CharField(max_length=120)
+    
+    def __unicode__(self):
+        return u"%d, %s" % (self.position, self.description)
     
     
 class Assignment(models.Model):
@@ -31,12 +35,15 @@ class AcceptanceCriteria(models.Model):
                       )
     standard = models.CharField(max_length=2,
                                 choices=STANDARD_CLASSIFICATION)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=120)
     expected_date = models.DateField()
     expected_percentage = models.IntegerField()
+    
+    def __unicode__(self):
+        return u"%s para %s terminar el %d antes del  %s" % (self.goal, self.standard, self.expected_percentage, self.expected_date)
     
 class Deliverable(models.Model):
     position = models.IntegerField()
     goal = models.ForeignKey(Goal)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=120)
     
