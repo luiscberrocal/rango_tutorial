5eb141ea4cad9cdcb7214f41a85a9f87826c87e4
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2013-11-26 23:02:21 -0500

    Added creator and created date to Page model

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
@@ -10,6 +11,8 @@ class Category(models.Model):
 
 class Page(models.Model):
 	category = models.ForeignKey(Category)
+	creator = models.ForeignKey(User, blank=True, null=True)
+	created = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=128)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
