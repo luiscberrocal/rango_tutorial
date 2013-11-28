dd9a185a1db855280330ee81c9f4457745e91ec7
Luis Berrocal <lberrocal@pancanal.com>
2013-11-28 08:19:02 -0500

    Added changes to Employee admin interface adn Assingment admin interface

--- goalmatrix/admin.py
+++ goalmatrix/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from goalmatrix.models import Employee, Goal, Assignment, AcceptanceCriteria, Deliverable, Team
 
-admin.site.register(Employee)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ["position", "goal", "weight"]
+    search_fields = ["goal"]
+
+class AssignmentInLine(admin.TabularInline):
+    model = Assignment
+    
+class EmployeeAdmin(admin.ModelAdmin):
+    inlines = [AssignmentInLine]
+    
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(Goal)
-admin.site.register(Assignment)
+admin.site.register(Assignment, AssignmentAdmin)
 admin.site.register(AcceptanceCriteria)
 admin.site.register(Deliverable)
 admin.site.register(Team)
