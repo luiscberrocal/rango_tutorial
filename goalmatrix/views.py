e820723a2346a9147cb4ced889e3b37dc20070ee
Luis Berrocal <lberrocal@pancanal.com>
2013-11-28 16:11:01 -0500

    Displaying assignments on web page

--- goalmatrix/views.py
+++ goalmatrix/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.views.generic.base import View
-from goalmatrix.models import Employee
+from goalmatrix.models import Employee, Assignment
 from django.template.context import RequestContext
 
 
 def showassignments(request, company_id):
     employee = Employee.objects.get(company_id=company_id)
+    #print "******************     %s        ***************************" % (employee.id)
+    #ddd = employee.assignment_set.all()
+    ddd = Assignment.objects.filter(employee__id=employee.id)
     context = RequestContext(request)
-    context_dict = {'employee': employee}
+    
+    context_dict = {'employee': employee, 'assignments' : ddd}
+    
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
