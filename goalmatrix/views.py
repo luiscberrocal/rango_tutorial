94ff26ab6d5dc857cea2699866e23505b022c7fc
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2013-12-01 13:38:27 -0500

    Grading is activated from the goal matrix. It now updates to the database

--- goalmatrix/views.py
+++ goalmatrix/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.views.generic.base import View
-from goalmatrix.models import Employee, Team
+from goalmatrix.models import Employee, Team, Goal
 from django.template.context import RequestContext
 import re
 from django.http.response import HttpResponse
+from django.core.urlresolvers import reverse
 
 
 
@@ -29,19 +30,34 @@ def manage_goal_matrix(request, username, action):
     employee = Employee.objects.get(username=username)
     context = RequestContext(request)
     context_dict = {'employee': employee}
-
+    if request.method == 'POST':
+        message = update_goals(request)
+        context_dict.update({'message' : message})
+        
     return render_to_response('goalmatrix/goal-matrix-form.html', context_dict, context)
 
 def update_goals(request):
     pattern = re.compile('grade-(\d*)$', re.UNICODE)
-    goal_ids = []
-    if request.method == 'POST':
-        for key, value in request.POST.iteritems():
-            m = pattern.match(key)
-            if m:
-                goal_ids.append((m.group(1), value))
-    goal_ids.append(('url', request.build_absolute_uri()))
-    return HttpResponse(goal_ids)
+    goal_ids = {}
+    for key, value in request.POST.iteritems():
+        m = pattern.match(key)
+        if m:
+            goal_ids.update({m.group(1):value})
+    goals = Goal.objects.filter(id__in = goal_ids.keys())
+    c=0
+    for goal in goals:
+        uni_goal_id = unicode(goal.id)
+        if goal.grade != goal_ids[uni_goal_id]:
+            c += 1
+            goal.grade = goal_ids[uni_goal_id]
+            goal.save()
+    
+            
+    
+    #goal_ids.append(('url', request.build_absolute_uri()))
+    #return HttpResponse(goal_ids)
+    #return manage_goal_matrix(request, request.POST.get('username'), 'edit', message=goal_ids)
+    return "Updated %d goals (%s)" % (c , goal_ids.keys())
             
             
             
